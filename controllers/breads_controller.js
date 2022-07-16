dd4f6fa9cc750f803bcbc320d454028aacab9584
KS Durazo <sdurazo.UNLV@gmail.com>
2022-07-16 10:29:48 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,17 +4,12 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('Index',
-      {
-        breads: Bread,
-        title: 'Index Page'
-      }
-    )
+  res.send('This is the index at /breads')
 })
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     res.send(Bread[req.params.arrayIndex])
-  })
-  
+  })  
+
 module.exports = breads
